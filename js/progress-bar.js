947f3ce738d895e88feec6b9395776d28bbb322e
Naman <namannayak.16@gmail.com>
2024-08-31 04:06:57 +0530

    fixed ui and page structure

---
+++ js/progress-bar.js
@@ -0,0 +1,10 @@
+
+window.onscroll = function() {scrollProgress()};
+
+function scrollProgress() {
+const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+const scrolled = (winScroll / height) * 100;
+document.getElementById("progress-bar").style.width = scrolled + "%";
+}
+
