3c494d4de4e371257fad5e791b8e4d8ca98a2027
Naman <namannayak.16@gmail.com>
2024-10-22 17:30:08 +0530

    bug fixes

--- js/Crypto Convertor/script.js
+++ js/Crypto Convertor/script.js
@@ -3,33 +3,42 @@ const toCurrencySelect = document.querySelector("#toCurrency");
 const convertBtn = document.querySelector('#convertBtn');
 
 (function makeOption() {
-    for (let key in cryptoList) {
-      let newOption = document.createElement("option");
-      newOption.innerText = cryptoList[key].toUpperCase();
-      newOption.value = key;
-      if (newOption.value === "BTC") {
-        newOption.selected = "selected";
-      }
-      fromCurrencySelect.appendChild(newOption);
+  for (let key in cryptoList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = cryptoList[key].toUpperCase();
+    newOption.value = key;
+    if (newOption.value === "BTC") {
+      newOption.selected = "selected";
     }
+    fromCurrencySelect.appendChild(newOption);
+  }
 
-    for (let key in cryptoList) {
-      let newOption = document.createElement("option");
-      newOption.innerText = cryptoList[key].toUpperCase();
-      newOption.value = key;
-      if (newOption.value === "ETH") {
-        newOption.selected = "selected";
-      }
-      toCurrencySelect.appendChild(newOption);
+  for (let key in cryptoList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = cryptoList[key].toUpperCase();
+    newOption.value = key;
+    if (newOption.value === "ETH") {
+      newOption.selected = "selected";
     }
+    toCurrencySelect.appendChild(newOption);
+  }
 })();
+
+const changeIcon = () => {
+  const sourceIcon = apiData.find(coin => coin.symbol === fromCurrencySelect.value.toLowerCase());
+  const targetIcon = apiData.find(coin => coin.symbol === toCurrencySelect.value.toLowerCase());
+}
 
 const convert = async () => {
   const sourceCurrency = cryptoList[fromCurrencySelect.value].toLowerCase();
   const targetCurrency = toCurrencySelect.value.toLowerCase();
   const amount = document.querySelector('#fromAmount').value;
+  if (isNaN(amount) || amount <= 0) {
+    alert("Please enter a valid amount.");
+    return; 
+  }
 
-  const URL = `https://api.coingecko.com/api/v3/simple/price?ids=${sourceCurrency}&vs_currencies=${targetCurrency}`
+  const URL = `https://api.coingecko.com/api/v3/simple/price?ids=${sourceCurrency}&vs_currencies=${targetCurrency}`;
 
   let response = await fetch(URL);
   let data = await response.json();
@@ -38,7 +47,6 @@ const convert = async () => {
   const result = amount * conversionRate;
 
   document.querySelector('#toAmount').value = result;
-  
 }
 
 function toggleRotate() {
@@ -48,8 +56,14 @@ function toggleRotate() {
   [fromCurrencySelect.value, toCurrencySelect.value] = [toCurrencySelect.value, fromCurrencySelect.value];
 }
 
-
-
-convertBtn.addEventListener('click', ()=>{
+convertBtn.addEventListener('click', () => {
+  renderImg();
   convert();
+});
+
+fromCurrencySelect.addEventListener('change', () => {
+  renderImg();
+});
+toCurrencySelect.addEventListener('change', () => {
+  renderImg();
 });
